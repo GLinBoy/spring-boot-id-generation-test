90a8af7e849e2fe9836c9f96a3539b61fa355ce5
GLinBoy <glinboy@live.com>
2025-01-18 17:27:15 +0100

    📦 NEW: Add a project to save entity by Java UUID

---
+++ javauuid/src/test/java/com/glinboy/sample/javauuid/JavauuidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.javauuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavauuidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
