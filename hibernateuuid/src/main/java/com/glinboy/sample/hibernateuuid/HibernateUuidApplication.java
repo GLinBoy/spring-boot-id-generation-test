bfdfcbc323295049cfc17d71c8ddbac3db3c42b7
GLinBoy <glinboy@live.com>
2025-01-18 17:26:22 +0100

    📦 NEW: Add a project to save entity by Hibernate UUID

---
+++ hibernateuuid/src/main/java/com/glinboy/sample/hibernateuuid/HibernateUuidApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.hibernateuuid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateUuidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateUuidApplication.class, args);
+	}
+
+}
---
+++ hibernateuuid/src/test/java/com/glinboy/sample/hibernateuuid/HibernateUuidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.hibernateuuid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateUuidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
