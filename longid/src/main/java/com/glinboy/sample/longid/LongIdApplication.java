60d7d9fb66d8d15db00bcdc44f9efb7fb162d6a5
GLinBoy <glinboy@live.com>
2025-01-18 17:23:47 +0100

    📦 NEW: Add a project to save entity by sequence ID generation

---
+++ longid/src/main/java/com/glinboy/sample/longid/LongIdApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.longid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongIdApplication.class, args);
+	}
+
+}
---
+++ longid/src/test/java/com/glinboy/sample/longid/LongIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.longid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
