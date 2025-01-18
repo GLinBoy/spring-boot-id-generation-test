d04935f1ff5c5f20fa3962f10d005595a4cefcfd
GLinBoy <glinboy@live.com>
2025-01-18 17:27:52 +0100

    📦 NEW: Add a project to save entity by NanoID

---
+++ nanoid/src/main/java/com/glinboy/sample/nanoid/NanoIdApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.nanoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoIdApplication.class, args);
+	}
+
+}
