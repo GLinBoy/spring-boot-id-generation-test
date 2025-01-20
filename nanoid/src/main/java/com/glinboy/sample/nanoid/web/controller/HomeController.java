250b45e4eb66913c964f5fa1fa55c33aab1bce01
GLinBoy <glinboy@live.com>
2025-01-20 10:42:07 +0100

    📦 NEW: Add a Home controller to redirect home request into SwaggerUI

---
+++ nanoid/src/main/java/com/glinboy/sample/nanoid/web/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.glinboy.sample.nanoid.web.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+}
