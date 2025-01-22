bff1fa126e021e21ead302bf137dd8c42d4250f4
GLinBoy <glinboy@live.com>
2025-01-22 16:12:06 +0100

    👌 IMPROVE: Enable AOP on Java UUID application

--- javauuid/src/main/java/com/glinboy/sample/javauuid/JavauuidApplication.java
+++ javauuid/src/main/java/com/glinboy/sample/javauuid/JavauuidApplication.java
@@ -2,8 +2,10 @@ package com.glinboy.sample.javauuid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class JavauuidApplication {
 
 	public static void main(String[] args) {
