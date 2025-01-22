779d95df5cbcd19e1e3fdb8d7566e660b2f3e337
GLinBoy <glinboy@live.com>
2025-01-22 16:11:38 +0100

    👌 IMPROVE: Enable AOP on Long ID application

--- longid/src/main/java/com/glinboy/sample/longid/LongIdApplication.java
+++ longid/src/main/java/com/glinboy/sample/longid/LongIdApplication.java
@@ -2,8 +2,10 @@ package com.glinboy.sample.longid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class LongIdApplication {
 
 	public static void main(String[] args) {
