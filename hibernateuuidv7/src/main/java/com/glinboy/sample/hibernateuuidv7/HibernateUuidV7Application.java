3b833a3b35848ddbd3c027837f80396d9e8076c8
GLinBoy <glinboy@live.com>
2025-11-23 13:30:46 +0100

    👌 IMPROVE: Format code for consistency and readability

--- hibernateuuidv7/src/main/java/com/glinboy/sample/hibernateuuidv7/HibernateUuidV7Application.java
+++ hibernateuuidv7/src/main/java/com/glinboy/sample/hibernateuuidv7/HibernateUuidV7Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HibernateUuidV7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HibernateUuidV7Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateUuidV7Application.class, args);
+    }
 
 }
