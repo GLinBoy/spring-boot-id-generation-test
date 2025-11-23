ea7b3461a9955fde8021362719b0b3cbab4a34df
GLinBoy <glinboy@live.com>
2025-11-23 13:22:44 +0100

    📦 NEW: Add a new project to configure Hibernate to use UUID version 7

---
+++ hibernateuuidv7/src/main/java/com/glinboy/sample/hibernateuuidv7/HibernateUuidV7Application.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.hibernateuuidv7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateUuidV7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateUuidV7Application.class, args);
+	}
+
+}
---
+++ hibernateuuidv7/src/test/java/com/glinboy/sample/hibernateuuidv7/HibernateUuidV7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.sample.hibernateuuidv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateUuidV7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
