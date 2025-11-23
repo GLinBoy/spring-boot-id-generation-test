000798c9dfa0f96c215e9cbd4801bcfb9ec4f7b6
GLinBoy <glinboy@live.com>
2025-11-23 13:30:59 +0100

    📦 NEW: Add ModelMapper configuration for object mapping

---
+++ hibernateuuidv7/src/main/java/com/glinboy/sample/hibernateuuidv7/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.glinboy.sample.hibernateuuidv7.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setAmbiguityIgnored(true);
+        return mapper;
+    }
+
+}
