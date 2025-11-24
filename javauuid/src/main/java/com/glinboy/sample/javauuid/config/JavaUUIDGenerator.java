190b3d26e9ba416426444e2974f0d795672fa2da
GLinBoy <glinboy@live.com>
2025-11-24 20:27:24 +0100

    📦 NEW: Refactor UUID generation by moving UUIDGenerator to a separate class

--- javauuid/src/main/java/com/glinboy/sample/javauuid/config/JavaUUIDGenerator.java
+++ javauuid/src/main/java/com/glinboy/sample/javauuid/config/JavaUUIDGenerator.java
@@ -1,16 +1,11 @@
 package com.glinboy.sample.javauuid.config;
 
-import lombok.extern.log4j.Log4j2;
-import lombok.val;
 import org.hibernate.annotations.IdGeneratorType;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.id.IdentifierGenerator;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.UUID;
 
 /**
  * Custom UUID generator annotation for Hibernate entities.
@@ -20,16 +15,5 @@ import java.util.UUID;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.METHOD})
 public @interface JavaUUIDGenerator {
-}
-
-@Log4j2
-class UUIDGenerator implements IdentifierGenerator {
-
-    @Override
-    public Object generate(SharedSessionContractImplementor session, Object object) {
-        val id = UUID.randomUUID();
-        log.info("Generated UUID: {}", id);
-        return id;
-    }
 }
 
---
+++ javauuid/src/main/java/com/glinboy/sample/javauuid/config/UUIDGenerator.java
@@ -0,0 +1,14 @@
+package com.glinboy.sample.javauuid.config;
+
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+import java.util.UUID;
+
+public class UUIDGenerator implements IdentifierGenerator {
+
+    @Override
+    public Object generate(SharedSessionContractImplementor session, Object object) {
+        return UUID.randomUUID();
+    }
+}
